5e6abb49aa21910f5228a8d250a35dc866abb89d
gregs <g.schrammel@icloud.com>
2022-02-24 00:44:18 -0300

    update rpc

--- lib/providers.ts
+++ lib/providers.ts
@@ -3,12 +3,16 @@ import { providers } from 'ethers'
 export const alchemy = process.env.NEXT_PUBLIC_ALCHEMY_ID as string
 export const etherscan = process.env.NEXT_PUBLIC_ETHERSCAN_API_KEY as string
 export const infuraId = process.env.NEXT_PUBLIC_INFURA_ID as string
+const concaveKey = process.env.NEXT_PUBLIC_CONCAVE_RPC_KEY
 
-export const concaveRPC = 'https://api.concave.lol/'
+export const concaveRPC = 'https://eth.concave.lol/'
 
 export const spoonProvider = (chainId: number) =>
   new providers.FallbackProvider([
-    new providers.JsonRpcProvider(concaveRPC, chainId),
+    new providers.JsonRpcProvider(
+      { url: concaveRPC, headers: { 'x-api-key': concaveKey } },
+      chainId,
+    ),
     providers.getDefaultProvider(chainId, { alchemy, etherscan, infuraId }),
   ])
 
